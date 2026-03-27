c0394ba18f3d4ce34cfa4fa161d44b91c2804d97
oofforbu <offorbuzordivine@gmail.com>
2026-03-27 09:06:30 +0100

    chapter2 - Introducing Go

---
+++ Introducing_Go/chapter1/exercise.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* A multi-line comment.
+	1. A whitespace is a newline in Go.
+	2. A comment is ignored by the Go compiler and is for your use or for the use of whoever comes across your source code.
+	3. The files in the fmt package would begin with package fmt.
+	4. To use the Exit function from os package, I would call the function from os package like this: os.Exit()
+	5. print "Hello, my name is (your name)"
+	End of comment. */
+
+	fmt.Println("Hello, my name is Divine.")
+}
