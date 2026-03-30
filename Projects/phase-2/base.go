d438f0d8d988bae6262ef3d56a18a5bc42533a40
oofforbu <offorbuzordivine@gmail.com>
2026-03-30 11:14:36 +0100

    Finished work on the Base Conversion Project

--- Projects/phase-2/base.go
+++ Projects/phase-2/base.go
@@ -29,7 +29,7 @@ func Base(s string) string {
 				return fmt.Sprintf("%s is not valid hex", str[i-1])
 
 			} else {
-				return fmt.Sprintf("Decimal: %d", temp)
+				return fmt.Sprintf("Decimal: %d", (temp))
 			}
 		} else if str[i] == "dec" {
 			temp, err := strconv.ParseInt(str[i-1], 10, 64)
@@ -38,12 +38,14 @@ func Base(s string) string {
 			} else {
 				num = strconv.FormatInt(temp, 2)
 				num2 = strconv.FormatInt(temp, 16)
+				res := fmt.Sprintf("Binary: %s", num)
+	            res2 := fmt.Sprintf("Hex: %s", strings.ToUpper(num2))
+
+	            return res + "\n" + res2
 			}
 		}
 	}
-	res := fmt.Sprintf("Binary: %s", num)
-	res2 := fmt.Sprintf("Hex: %s", num2)
-	return res + "\n" + res2
+	return fmt.Sprintf("%s does not include a valid decimal, hex or binary. Check input and try again.\n", s)
 }
 
 func main() {
