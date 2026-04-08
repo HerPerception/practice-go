a66b53f31a687ebd0cd935b171962c5e48910b3f
oofforbu <offorbuzordivine@gmail.com>
2026-04-08 11:24:00 +0100

    Studies on the Go-Reloaded Project

---
+++ Go-Reloaded/cmd.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func CmdN(s []string) []string {
+	for i := 0; i < len(s); i++ {
+		num := 0
+		cmd := ""
+		if strings.HasPrefix(s[i], "(") && strings.HasSuffix(s[i+1], ")") {
+			cmd = s[i][1:]
+			temp, err := strconv.Atoi(s[i+1][:len(s[i+1])-1])
+			if err == nil {
+				num = temp
+			}
+		}
+		for j := i - num; j < i; j++ {
+			if cmd == "up" {
+				s[j] = strings.ToUpper(s[j])
+			} else if cmd == "low" {
+				s[j] = strings.ToLower(s[j])
+			} else if cmd == "cap" {
+				s[j] = strings.ToUpper(string(s[j][0])) + strings.ToLower(s[j][1:])
+			}
+		}
+		if strings.HasPrefix(s[i], "(") {
+			s = append(s[:i], s[i+1:]...)
+			i--
+		} else if strings.HasSuffix(s[i], ")") {
+			s = append(s[:i], s[i+1:]...)
+			i--
+		}
+	}
+	return s
+}
+
+func main() {
+	fmt.Println(CmdN([]string{"my", "mum", "(up", "2)", "iS", "VERY", "NICE", "(low", "3)", "and", "my", "dad", "too", "(cap", "4)"}))
+}
---
+++ Go-Reloaded/upn.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func UpN(s []string) []string {
+	cmd := ""
+	num := 1
+	//temp := 0
+	for i := 0; i < len(s); i++ {
+		if strings.HasPrefix(s[i], "(") && strings.HasSuffix(s[i+1], ")") {
+			cmd = s[i][1:]
+			num = int(s[i+1][0] - '0')
+			//temp = i - num
+			if num > len(s) {
+				num = len(s)
+			}
+
+			for j := i - num; j < i; j++ {
+				if cmd == "up" {
+					s[j] = strings.ToUpper(s[j])
+				} else if cmd == "low" {
+					s[j] = strings.ToLower(s[j])
+				} else if cmd == "cap" {
+					s[j] = strings.ToUpper(string(s[j][0])) + strings.ToLower(s[j][1:])
+				}
+			}
+		}
+		if strings.HasPrefix(s[i], "(") {
+			s = append(s[:i], s[i+1:]...)
+			i--
+		} else if strings.HasSuffix(s[i], ")") {
+			s = append(s[:i], s[i+1:]...)
+			i--
+		}
+
+	}
+	return s
+}
+
+func main() {
+	fmt.Println(UpN([]string{"my", "mum", "(up", "2)", "iS", "VERY", "NICE", "(low", "3)", "and", "my", "dad", "too", "(cap", "4)"}))
+}
