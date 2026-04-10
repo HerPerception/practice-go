394b6a0a1bfcd194b2fea5f71e6cf432f8c50997
oofforbu <offorbuzordivine@gmail.com>
2026-04-10 12:01:30 +0100

    Studies on ASCII Art - Mapping Logic

---
+++ ASCII-Practice/map-logic.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func getCharLines(lines []string, index int) []string {
+	start := index*4 + 1
+	end := start + 3
+	if end > len(lines) {
+		return []string{}
+	}
+	return lines[start:end]
+}
+
+func main() {
+	lines := []string{"", "A1", "A2", "A3", "", "B1", "B2", "B3"}
+	fmt.Println(getCharLines(lines, 0))
+	fmt.Println(getCharLines(lines, 1))
+}
