3f2f177eeb7169d5e5a5869d5fdcb4e7659cc4a8
HerPerception <offorbuzor1@gmail.com>
2026-05-09 21:44:35 +0100

    QuadB and QuadC

--- quad-go/main.go
+++ quad-go/main.go
@@ -1,24 +1,5 @@
 package main
 
-import "fmt"
-
-func QuadA(x, y int) {
-	for i := 0; i <= y-1; i++ {
-		for j := 0; j <= x-1; j++ {
-			if j != 0 && j != x-1 && (i == 0 || i == y-1) {
-				fmt.Print("-")
-			} else if i == 1 && (j == 0 || j == x-1) {
-				fmt.Print("|")
-			} else if i == 1 && (j != 0 || j != x-1) {
-				fmt.Print(" ")
-			} else if (j == 0 || j == 4 && i == 0) || (j == 0 || j == x-1 && i == y-1) {
-				fmt.Print("o")
-			}
-		}
-		fmt.Println()
-	}
-}
-
 func main() {
-	QuadA(5, 5)
+	QuadA(5, 3)
 }
